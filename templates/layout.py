9c792e3112cfca3ef323853c7c13bf36c13e4638
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-04-12 12:46:45 +0530

    Update and rename index.html to layout.py

---
+++ templates/layout.py
@@ -0,0 +1,17 @@
+def draw_border(c):
+    width, height = c._pagesize
+
+    margin = 20
+    c.rect(margin, margin, width - 2*margin, height - 2*margin)
+
+
+def draw_title_block(c):
+    c.setFont("Helvetica", 10)
+
+    # Bottom right title block
+    c.rect(800, 20, 300, 120)
+
+    c.drawString(810, 120, "SHREE CHAMUNDA DIE & ENGINEERS")
+    c.drawString(810, 100, "MOULD DESIGN")
+    c.drawString(810, 80, "DRAWING TYPE: BRICK MOULD")
+    c.drawString(810, 60, "UNIT: MM")
