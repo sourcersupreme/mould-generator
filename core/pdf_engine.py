50540b0433806668f7b20d5633f72398768286f0
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-04-12 12:49:17 +0530

    Update pdf_engine.py

--- core/pdf_engine.py
+++ core/pdf_engine.py
@@ -2,8 +2,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A3
 
 def create_canvas(filename):
-    c = canvas.Canvas(filename, pagesize=A3)
-    return c
+    return canvas.Canvas(filename, pagesize=A3)
 
 def save_canvas(c):
     c.save()
